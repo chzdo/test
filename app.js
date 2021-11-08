32109a40b7240a76cda507b780241fbf12393c03
Stanley Nduaguibe <snduaguibe@atbtechsoft.com>
2021-11-08 23:34:43 +0100

    hello

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(require("cors")());
 
 app.get("/", (r, j) => j.status(200).send("this is just a tes"));
 
-app
+
 app.listen(process.env.PORT || 5000, () => console.log(`port -${process.env.PORT}`))
 
