38187377f00510c4788f240124632adc90c6699f
Stanley Nduaguibe <snduaguibe@atbtechsoft.com>
2021-11-09 09:41:26 +0100

    hello

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cron = require("node-cron");
+const http = require("http");
+const axios = require("axios");
+cron.schedule("30 * * * *", () => {
+    console.log("running cron")
+    axios.get("https://starswarapp.herokuapp.com").then((data)=> console.log(data))
+})
 
 
 const app = express();
 
-app.use(require("cors")());
 
-
-app.get("/", (r, j) => j.status(200).send("this is just a tes"));
 
 
 app.listen(process.env.PORT || 5000, () => console.log(`port -${process.env.PORT}`))
